3992f368767f05be62d2807633ea16f1af6cd72b
JAHIN IMTIAZ <jimtiaz@cs.stonybrook.edu>
2020-03-20 18:37:43 -0400

    sf_malloc done except extending mem_grow part

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -16,12 +16,16 @@ size_t get_required_block_bize(size_t size);
 void set_heap();
 void add_wilderness_block(sf_block *block);
 void add_free_block(sf_block *block);
-void free_list_init();
+void free_list_heads_init();
+void free_list_helper_init();
 sf_block *find_fit(size_t size);
 void split_block(sf_block *bp, size_t size);
+int find_free_list_index(size_t msize);
+
 char *my_heap;
 char *prologue_header;
 char *epilogue_header;
+size_t sf_free_list_helper[NUM_FREE_LISTS];
 
 void *sf_malloc(size_t size) {
     sf_block *bp;
@@ -32,10 +36,12 @@ void *sf_malloc(size_t size) {
             return NULL;
         }
         else{
-            //initialize the free list
-            free_list_init();
-            //set up heap and add unused block in free list
-            set_heap();
+
+            free_list_heads_init();     //initialize the free list
+
+            free_list_helper_init();    //initialize the free list helper
+
+            set_heap();     //set up heap and add unused block in free list
         }
     }
 
@@ -68,6 +74,7 @@ void *sf_malloc(size_t size) {
     }
 
     //No block found. extend memory and place the block
+
 }
 
 void sf_free(void *ptr) {
@@ -111,7 +118,7 @@ void set_heap(){
 }
 
 //initialize the free list
-void free_list_init(){
+void free_list_heads_init(){
     int i;
     for(i = 0; i < NUM_FREE_LISTS; i++){
         sf_block sentinel;
@@ -121,6 +128,16 @@ void free_list_init(){
     }
 }
 
+//initialize the free list helper with fiboncacci numbers
+void free_list_helper_init(){
+    sf_free_list_helper[0] = 1;
+    sf_free_list_helper[1] = 2;
+
+    size_t i;
+    for(i = 2; i < NUM_FREE_LISTS - 1; i++){
+        sf_free_list_helper[i] = sf_free_list_helper[i-1] + sf_free_list_helper[i-2];
+    }
+}
 
 //find free block in the list
 //returns a pointer to the block
@@ -189,7 +206,36 @@ void add_wilderness_block(sf_block *block){
 
 //add a free block in the free list
 void add_free_block(sf_block *block){
-    //get the size of the block
-    //find the appropriate size class for that block
+
+    size_t msize = GET_SIZE((char *)block + 8);//get the size of the block
+    msize = msize / 64;     //turn it into m value
+    int index = find_free_list_index(msize);//find the appropriate size class for that block
+
     //insert it in the list
+    (*block).body.links.next = sf_free_list_heads[index].body.links.next;
+
+    (*block).body.links.prev = &sf_free_list_heads[index];
+
+    (*(*block).body.links.next).body.links.prev = block;
+
+    sf_free_list_heads[index].body.links.next = block;
+}
+
+//find the appropriate size class given the MSIZE and return the index
+int find_free_list_index(size_t msize){
+
+    if( msize > 0 && msize <= 3){   //return (msize-1) if it is 1,2 or 3
+        return (int) (msize -1);
+    }
+    else{   //else return index of appropriate size class
+        int i;
+        for(i = 3; i <= (NUM_FREE_LISTS - 3); i++){
+            if((msize > sf_free_list_helper[i-1]) && (msize <= sf_free_list_helper[i])){
+                return i;
+            }
+        }
+        //for all block that are too big to fit into any size class, put them in
+        //second to last list of the free list
+        return NUM_FREE_LISTS-2;
+    }
 }
