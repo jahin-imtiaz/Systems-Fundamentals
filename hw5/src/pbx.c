1bf3cf1787b11eb468a700cc3648316efe2901c7
JAHIN IMTIAZ <jimtiaz@cs.stonybrook.edu>
2020-05-04 23:51:01 -0400

    Some pbx functions are done

--- hw5/src/pbx.c
+++ hw5/src/pbx.c
@@ -17,6 +17,7 @@ struct tu{
     int current_state;
     int extention;
     int fileno;
+    struct tu *ringing_tu;
     struct tu *connected_tu;
 
 };
@@ -37,7 +38,7 @@ TU *pbx_register(PBX *pbx, int fd){
 
     TU *tu = malloc(sizeof(TU));
 
-    if(tu == NULL){
+    if((pbx == NULL) || (tu == NULL)){
         return NULL;
     }
     else{
@@ -56,51 +57,186 @@ TU *pbx_register(PBX *pbx, int fd){
 
 }
 
-int pbx_unregister(PBX *pbx, TU *tu);
-int tu_fileno(TU *tu);
-int tu_extension(TU *tu);
-int tu_pickup(TU *tu);
-int tu_hangup(TU *tu);
+int pbx_unregister(PBX *pbx, TU *tu){
+
+    //USE MUTEX
+    if((pbx == NULL) || (tu == NULL) || (pbx->extension_array[tu->extention] == NULL)){
+        return -1;
+    }
+    else{
+
+        pbx->extension_array[tu->extention] = NULL;
+        free(tu);
+        return 0;
+
+    }
+    //USE MUTEX
+
+}
+
+int tu_fileno(TU *tu){
+
+    if(tu != NULL){
+        return tu->fileno;
+    }
+    else{
+        return -1;
+    }
+}
+
+int tu_extension(TU *tu){
+
+    if(tu != NULL){
+        return tu->extention;
+    }
+    else{
+        return -1;
+    }
+}
+
+int tu_pickup(TU *tu){
+
+    if(tu == NULL){
+        return -1;
+    }
+    else{
+        if(tu->current_state == TU_ON_HOOK){
+
+            tu->current_state = TU_DIAL_TONE;
+            send_notification(TU_DIAL_TONE, tu->fileno, 0);
+
+        }
+        else if(tu->current_state == TU_RINGING){
+
+            tu->current_state = TU_CONNECTED;
+            tu->connected_tu = tu->ringing_tu;
+            send_notification(TU_CONNECTED, tu->fileno, (tu->ringing_tu)->extention);
+
+            (tu->ringing_tu)->current_state = TU_CONNECTED;
+            (tu->ringing_tu)->connected_tu = tu;
+            send_notification(TU_CONNECTED, (tu->ringing_tu)->fileno, tu->extention);
+
+        }
+        else{
+            send_notification(tu->current_state, tu->fileno, 0);
+        }
+
+        return 0;
+    }
+
+}
+
+int tu_hangup(TU *tu){
+
+    if(tu == NULL){
+        return -1;
+    }
+    else{
+
+        if(tu->current_state == TU_CONNECTED){
+
+            tu->current_state = TU_ON_HOOK;
+
+            (tu->connected_tu)->current_state = TU_DIAL_TONE;
+
+            send_notification(TU_ON_HOOK, tu->fileno, 0);
+            send_notification(TU_DIAL_TONE, (tu->connected_tu)->fileno, 0);
+
+            (tu->connected_tu)->ringing_tu = NULL;
+            (tu->connected_tu)->connected_tu = NULL;
+
+            tu->ringing_tu = NULL;
+            tu->connected_tu = NULL;
+
+        }
+        else if(tu->current_state == TU_RING_BACK){
+
+            tu->current_state = TU_ON_HOOK;
+
+            (tu->ringing_tu)->current_state = TU_ON_HOOK;
+
+            send_notification(TU_ON_HOOK, tu->fileno, 0);
+            send_notification(TU_ON_HOOK, (tu->ringing_tu)->fileno, 0);
+
+            (tu->ringing_tu)->ringing_tu = NULL;
+            tu->ringing_tu = NULL;
+
+        }
+        else if(tu->current_state == TU_RINGING){
+
+            tu->current_state = TU_ON_HOOK;
+
+            (tu->ringing_tu)->current_state = TU_DIAL_TONE;
+
+            send_notification(TU_ON_HOOK, tu->fileno, 0);
+            send_notification(TU_DIAL_TONE, (tu->ringing_tu)->fileno, 0);
+
+            (tu->ringing_tu)->ringing_tu = NULL;
+            tu->ringing_tu = NULL;
+
+        }
+        else if(tu->current_state == TU_DIAL_TONE || tu->current_state == TU_BUSY_SIGNAL || tu->current_state == TU_ERROR){
+
+            tu->current_state = TU_ON_HOOK;
+            send_notification(TU_ON_HOOK, tu->fileno, 0);
+
+        }
+        else{
+
+            send_notification(tu->current_state, tu->fileno, 0);
+
+        }
+
+        return 0;
+    }
+
+}
+
 int tu_dial(TU *tu, int ext);
 int tu_chat(TU *tu, char *msg);
 
 void send_notification(int state, int fd, int connected_tu){
 
+    //TAKE CARE OF WHICH TO USE: DPRINTF() OR SPRINTF()
     char *buf;
     switch(state){
 
         case TU_ON_HOOK:
             buf = malloc(30);
-            sprintf(buf, "%s %d", tu_state_names[state], state);
+            sprintf(buf, "%s %d%s", tu_state_names[state], fd, EOL);
             write(fd, buf, strlen(buf));
             free(buf);
             break;
 
         case TU_RINGING:
             write(fd, tu_state_names[state], strlen(tu_state_names[state]));
+            write(fd, EOL, strlen(EOL));
             break;
         case TU_DIAL_TONE:
             write(fd, tu_state_names[state], strlen(tu_state_names[state]));
+            write(fd, EOL, strlen(EOL));
             break;
         case TU_RING_BACK:
             write(fd, tu_state_names[state], strlen(tu_state_names[state]));
+            write(fd, EOL, strlen(EOL));
             break;
         case TU_BUSY_SIGNAL:
             write(fd, tu_state_names[state], strlen(tu_state_names[state]));
+            write(fd, EOL, strlen(EOL));
             break;
 
         case TU_CONNECTED:
             buf = malloc(30);
-            sprintf(buf, "%s %d", tu_state_names[state], connected_tu);
+            sprintf(buf, "%s %d%s", tu_state_names[state], connected_tu, EOL);
             write(fd, buf, strlen(buf));
             free(buf);
             break;
 
         case TU_ERROR:
             write(fd, tu_state_names[state], strlen(tu_state_names[state]));
+            write(fd, EOL, strlen(EOL));
             break;
 
     }
-    /*write(fd, string, strlen(string));*/
 
 }
